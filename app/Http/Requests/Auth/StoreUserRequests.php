122fe47521e2167a2aaa233a79e2f26679759da8
鈴木よつば <>
2025-11-24 21:57:12 +0900

    訂正

--- app/Http/Requests/Auth/StoreUserRequests.php
+++ app/Http/Requests/Auth/StoreUserRequests.php
@@ -18,13 +18,12 @@ public function authorize(): bool
             'over_name' => ['required','string','max:10'],
             'under_name' => ['required','string','max:10'],
             'over_name_kana' =>
-            ['required','string','regex:/^[ア-ン゛゜ァ-ォ ャ-ョー]+$/u','max:30'],
+            ['required','string','regex:/^[ア-ン゛゜ァ-ォャ-ョー]+$/u','max:30'],
             'under_name_kana' =>
-            ['required','string','regex:/^[ア-ン゛゜ァ-ォ ャ-ョー]+$/u','max:30'],
+            ['required','string','regex:/^[ア-ン゛゜ァ-ォャ-ョー]+$/u','max:30'],
 
             'mail_address' =>
             ['required',//入力必須
-            'mail_address' => 'email',//メールアドレス形式
             'max:100',
             'unique:users,email'],
 
@@ -38,7 +37,7 @@ public function authorize(): bool
                 'required',
                 'min:8',
                 'max:30',
-                'password' => 'confirmed:password',  // password_confirmation と一致しているか
+                'confirmed',  // password_confirmation と一致しているか
             ],
         ];
     }
@@ -50,11 +49,12 @@ public function authorize(): bool
         'under_name.required' => 'ユーザー名は必須です。',
         'under_name.max' => 'ユーザー名は10文字以内で入力してください。',
         'over_name_kana.required' => 'ユーザー名は必須です。',
-        'over_name_kana.regex:/^[ア-ン゛゜ァ-ォ ャ-ョー]+$/u' => 'パスワードはカタカナのみで入力してください。',
+        'over_name_kana.regex' => 'セイはカタカナのみで入力してください。',
         'over_name_kana.max' => 'ユーザー名は30文字以内で入力してください。',
         'under_name_kana.required' => 'ユーザー名は必須です。',
-        'under_name_kana.regex:/^[ア-ン゛゜ァ-ォ ャ-ョー]+$/u' => 'パスワードはカタカナのみで入力してください。',
-        'under_name_kana_kana.max' => 'ユーザー名は30文字以内で入力してください。',
+        'under_name_kana.regex' => 'メイはカタカナのみで入力してください。',
+        'under_name_kana.string' => 'メイは文字列で入力してください。',
+        'under_name_kana.max' => 'ユーザー名は30文字以内で入力してください。',
 
         'mail_address.required' => 'メールアドレスは必須です。',
         'mail_address.email' => '正しいメールアドレス形式で入力してください。',
@@ -65,9 +65,9 @@ public function authorize(): bool
         'old_month.required' =>'生年月日は入力必須です。',
         'old_day.required' =>'生年月日は入力必須です。',
         'sex.required' =>'性別は入力必須です。',
-        'sex.in:男,女,その他'=>'性別は男、女、その他で入力してください。',
+        'sex.in'=>'性別は男、女、その他で入力してください。',
         'role.required'=>'入力必須です。',
-        'role.in:講師(国語),講師(英語),講師(数学),生徒'=>'項目以外選択禁止',
+        'role.in'=>'項目以外選択禁止',
         'password.required' => 'パスワードは必須です。',
         'password.min' => 'パスワードは8文字以上で入力してください。',
         'password.max' => 'パスワードは30文字以内で入力してください。',
